751bce06268ae6de7006e4b6e121249d5d625b6b
BoonRealCuaaaaa <99378331+BoonRealCuaaaaa@users.noreply.github.com>
2024-05-09 09:42:12 +0700

    add up to 18

---
+++ 13-user-input-handling/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header.jsx';
+import Login from './components/ogin.jsx';
+import Signup from './components/Singup.jsx';
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Signup/>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ 18-authentication/frontend/src/pages/Root.js
@@ -0,0 +1,38 @@
+import { Outlet, useLoaderData } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  const submit = useSubmit();
+  const token = useLoaderData();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
